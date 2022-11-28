b30cd4e76ad952f3b57ef3cd5f7ac1e4ea4876ad
gunamari <gunamani0303@gmail.com>
2022-11-28 22:09:50 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+ function mul(number){
+    return new Promise((resolve,reject)=>setTimeout(()=>{
+        resolve(2*number)
+    },3000));
+ }
+ function bar(number){
+    return new Promise((resolve,reject)=>setTimeout(()=>{
+        reject(2*number+"this is rejected")
+    },3000));
+ }
+ mul(12)
+ .then((data)=>{
+    console.log(data);
+    return mul(data);
+ }).then((data1)=>{
+    console.log(data1);
+    return bar(data1);
+ }).then((data2)=>console.log(data2)).catch((error)=>console.log(error));
